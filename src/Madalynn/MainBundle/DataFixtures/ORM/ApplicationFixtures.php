aa214cec2ca15dcdee7bb4dba37c4c210b7f8c62
Sébastien Brochet <blinkseb@androirc.com>
2012-01-26 17:15:05 +0100

    changed: new coding conventions

--- src/Madalynn/MainBundle/DataFixtures/ORM/ApplicationFixtures.php
+++ src/Madalynn/MainBundle/DataFixtures/ORM/ApplicationFixtures.php
@@ -3,78 +3,79 @@
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-
 use Madalynn\MainBundle\Entity\User;
-
 use Faker\Factory as FakerFactory;
 
-class ApplicationFixtures implements FixtureInterface, ContainerAwareInterface {
+class ApplicationFixtures implements FixtureInterface, ContainerAwareInterface
+{
 
-  private $container;
+    private $container;
 
-  public function setContainer(ContainerInterface $container = null) {
-    $this->container = $container;
-  }
-
-  public function load($manager) {
-    $faker = FakerFactory::create("fr_FR");
-    $userManager = $this->container->get('fos_user.user_manager');
-
-    for ($i = 0; $i < 30; $i++) {
-      $user = new User();
-
-      $user->setUsername($faker->userName);
-      $user->setEmail($faker->email);
-      $user->setFirstname($faker->firstName);
-      $user->setLastname($faker->lastName);
-      $user->setGroup($faker->word);
-      $user->setLevel($faker->randomElement(array(-1, 0, 1, 2, 3, 4, 5)));
-      $user->setPhone($faker->phoneNumber);
-      $user->setStatus($faker->randomElement(array("DOCTORANT", "EX-DOCTORANT")));
-      $user->setBirthday($faker->dateTimeThisCentury);
-      $user->setBeginningyear($faker->dateTimeThisDecade);
-      $user->setSoutenance($faker->dateTimeBetween('now', '+10 years'));
-      $user->setPlainPassword($faker->lexify("??????"));
-      $user->setThesis($faker->sentence(10));
-      $user->setOffice($faker->randomNumber(3));
-      $user->setBuilding("Dirac");
-
-      $userManager->updateUser($user);
-
-      $article = new Madalynn\MainBundle\Entity\Article();
-      $article->setAuthor($user);
-      $article->setContent($faker->paragraph(3));
-      $article->setTitle($faker->sentence);
-      $article->setSlug($faker->word);
-      $article->setVisible(true);
-
-      $manager->persist($article);
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 
-    $user = new User();
+    public function load($manager)
+    {
+        $faker = FakerFactory::create("fr_FR");
+        $userManager = $this->container->get('fos_user.user_manager');
 
-    $user->setUsername('blinkseb');
-    $user->setEmail('blinkseb@gmail.com');
-    $user->setFirstname('Sébastien');
-    $user->setLastname('Brochet');
-    $user->setGroup('CMS');
-    $user->setLevel('1');
-    $user->setPhone($faker->phoneNumber);
-    $user->setStatus("DOCTORANT");
-    $user->setBirthday(new \DateTime('1987-04-22'));
-    $user->setBeginningyear(new \DateTime('2011-10-01'));
-    $user->setSoutenance(new \DateTime('2014-10-01'));
-    $user->setPlainPassword('btnk62');
-    $user->setThesis('Blabla avec CMS');
-    $user->setOffice(116);
-    $user->setBuilding("Dirac");
-    $user->setEnabled(true);
-    $user->setSuperAdmin(true);
+        for ($i = 0; $i < 30; $i++) {
+            $user = new User();
 
-    $userManager->updateUser($user);
+            $user->setUsername($faker->userName);
+            $user->setEmail($faker->email);
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setGroup($faker->word);
+            $user->setLevel($faker->randomElement(array(-1, 0, 1, 2, 3, 4, 5)));
+            $user->setPhone($faker->phoneNumber);
+            $user->setStatus($faker->randomElement(array("DOCTORANT", "EX-DOCTORANT")));
+            $user->setBirthday($faker->dateTimeThisCentury);
+            $user->setBeginningyear($faker->dateTimeThisDecade);
+            $user->setSoutenance($faker->dateTimeBetween('2009-10-01', '2012-09-30'));
+            $user->setPlainPassword($faker->lexify("??????"));
+            $user->setThesis($faker->sentence(10));
+            $user->setOffice($faker->randomNumber(3));
+            $user->setBuilding("Dirac");
 
-    $manager->flush();
-  }
+            $userManager->updateUser($user);
+
+            $article = new Madalynn\MainBundle\Entity\Article();
+            $article->setAuthor($user);
+            $article->setContent($faker->paragraph(3));
+            $article->setTitle($faker->sentence);
+            $article->setSlug($faker->word);
+            $article->setVisible(true);
+
+            $manager->persist($article);
+        }
+
+        $user = new User();
+
+        $user->setUsername('blinkseb');
+        $user->setEmail('blinkseb@gmail.com');
+        $user->setFirstname('Sébastien');
+        $user->setLastname('Brochet');
+        $user->setGroup('CMS');
+        $user->setLevel('1');
+        $user->setPhone($faker->phoneNumber);
+        $user->setStatus("DOCTORANT");
+        $user->setBirthday(new \DateTime('1987-04-22'));
+        $user->setBeginningyear(new \DateTime('2011-10-01'));
+        $user->setSoutenance(new \DateTime('2014-10-01'));
+        $user->setPlainPassword('annyes');
+        $user->setThesis('Blabla avec CMS');
+        $user->setOffice(116);
+        $user->setBuilding("Dirac");
+        $user->setEnabled(true);
+        $user->setSuperAdmin(true);
+
+        $userManager->updateUser($user);
+
+        $manager->flush();
+    }
 
 }
 
